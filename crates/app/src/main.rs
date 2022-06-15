c3efc9370e20589683578c1474a6a0df1dc66b56
brynnbrancamp <brynnbrancamp@gmail.com>
2022-06-15 18:06:08 -0500

    Added vk debug utils.

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,9 +1,42 @@
 mod window;
 
 use crate::window::{Event as WindowEvent, Window};
+use log::{error, info, trace, warn};
+
+fn debug_utils_messenger_callback(data: &vk::DebugUtilsMessengerCallbackData) -> bool {
+    match data.message_severity {
+        vk::DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE => trace!("{}", data.message),
+        vk::DEBUG_UTILS_MESSAGE_SEVERITY_INFO => info!("{}", data.message),
+        vk::DEBUG_UTILS_MESSAGE_SEVERITY_WARNING => warn!("{}", data.message),
+        vk::DEBUG_UTILS_MESSAGE_SEVERITY_ERROR => error!("{}", data.message),
+        _ => panic!("unrecognized message severity"),
+    }
+
+    false
+}
+
+struct Logger;
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        metadata.level() <= log::Level::Info
+    }
+
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+    fn flush(&self) {}
+}
+
+static LOGGER: Logger = Logger;
 
 fn main() {
     println!("Hello, world!");
+
+    log::set_max_level(log::LevelFilter::Trace);
+    log::set_logger(&LOGGER);
 
     let mut window = Window::new();
 
@@ -21,19 +54,38 @@ fn main() {
     let mut extensions = vec![vk::KHR_SURFACE, vk::KHR_XLIB_SURFACE];
     let mut layers = vec![];
 
+    let mut debug_utils_messenger_create_info = None;
+
     #[cfg(debug_assertions)]
     {
-        extensions.push(vk::EXT_DEBUG_REPORT);
+        extensions.push(vk::EXT_DEBUG_UTILS);
         layers.push(vk::LAYER_KHRONOS_VALIDATION);
+
+        debug_utils_messenger_create_info = Some(vk::DebugUtilsMessengerCreateInfo {
+            message_severity: vk::DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE
+                | vk::DEBUG_UTILS_MESSAGE_SEVERITY_INFO
+                | vk::DEBUG_UTILS_MESSAGE_SEVERITY_WARNING
+                | vk::DEBUG_UTILS_MESSAGE_SEVERITY_ERROR,
+            message_type: vk::DEBUG_UTILS_MESSAGE_TYPE_GENERAL
+                | vk::DEBUG_UTILS_MESSAGE_TYPE_VALIDATION
+                | vk::DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE,
+            user_callback: debug_utils_messenger_callback,
+        });
     }
 
-    let create_info = vk::InstanceCreateInfo {
+    let instance_create_info = vk::InstanceCreateInfo {
         application_info: &application_info,
         extensions: &extensions[..],
         layers: &layers[..],
+        debug_utils: debug_utils_messenger_create_info,
     };
 
-    let instance = vk::create_instance(create_info).expect("failed to create instance");
+    let instance = vk::create_instance(instance_create_info).expect("failed to create instance");
+
+    #[cfg(debug_assertions)]
+    let debug_utils_messenger =
+        vk::create_debug_utils_messenger(instance, debug_utils_messenger_create_info.unwrap())
+            .expect("failed to create debug utils messenger");
 
     loop {
         let event = window.next_event();
--- libs/vk/src/lib.rs
+++ libs/vk/src/lib.rs
@@ -1,9 +1,12 @@
-use std::ffi::CString;
-use std::mem::MaybeUninit;
+use std::ffi::{CStr, CString};
+use std::mem::{self, MaybeUninit};
 use std::ptr;
 
 mod ffi {
-    pub use libc::{c_char, c_int, c_long, c_uint, c_ulong, c_void};
+    use std::ffi::{CStr, CString};
+    use std::mem;
+
+    use libc::{c_char, c_int, c_long, c_uint, c_ulong, c_void};
 
     #[derive(Clone, Copy, Debug)]
     #[repr(C)]
@@ -34,6 +37,7 @@ mod ffi {
     pub enum StructureType {
         ApplicationInfo = 0,
         InstanceCreateInfo = 1,
+        DebugUtilsMessengerCreateInfo = 1000128004,
     }
 
     #[derive(Clone, Copy)]
@@ -63,7 +67,126 @@ mod ffi {
 
     #[derive(Clone, Copy)]
     #[repr(C)]
-    pub struct Instance([u8; 0]);
+    pub struct Instance(*mut u8);
+
+    #[derive(Clone, Copy)]
+    #[repr(transparent)]
+    pub struct DebugUtilsMessenger(u64);
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub enum ObjectType {
+        Unknown = 0,
+        Instance = 1,
+        PhysicalDevice = 2,
+        Device = 3,
+        Queue = 4,
+        Semaphore = 5,
+        CommandBuffer = 6,
+        Fence = 7,
+        DeviceMemory = 8,
+        Buffer = 9,
+        Image = 10,
+        Event = 11,
+        QueryPool = 12,
+        BufferView = 13,
+        ImageView = 14,
+        ShaderModule = 15,
+        PipelineCache = 16,
+        PipelineLayout = 17,
+        RenderPass = 18,
+        Pipeline = 19,
+        DescriptorSetLayout = 20,
+        Sampler = 21,
+        DescriptorPool = 22,
+        DescriptorSet = 23,
+        Framebuffer = 24,
+        CommandPool = 25,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct DebugUtilsLabel {
+        pub structure_type: StructureType,
+        pub p_next: *const c_void,
+        pub label_name: *const c_char,
+        pub color: [f32; 4],
+    }
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct DebugUtilsObjectNameInfo {
+        pub structure_type: StructureType,
+        pub p_next: *const c_void,
+        pub object_type: ObjectType,
+        pub object_handle: c_ulong,
+        pub object_name: *const c_char,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct DebugUtilsMessengerCallbackData {
+        pub structure_type: StructureType,
+        pub p_next: *const c_void,
+        pub flags: c_uint,
+        pub message_id_name: *const c_char,
+        pub message_id_number: c_int,
+        pub message: *const c_char,
+        pub queue_label_count: c_uint,
+        pub queue_labels: *const DebugUtilsLabel,
+        pub cmd_buf_label_count: c_uint,
+        pub cmd_buf_labels: *const DebugUtilsLabel,
+        pub object_count: c_uint,
+        pub objects: *const DebugUtilsObjectNameInfo,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct DebugUtilsMessengerCreateInfo {
+        pub structure_type: StructureType,
+        pub p_next: *const c_void,
+        pub flags: c_uint,
+        pub message_severity: c_int,
+        pub message_type: c_int,
+        pub user_callback: DebugUtilsMessengerCallbackInternal,
+        pub user_data: *const c_void,
+    }
+
+    pub type DebugUtilsMessengerCallbackInternal = unsafe extern "system" fn(
+        c_uint,
+        c_uint,
+        *const DebugUtilsMessengerCallbackData,
+        *const c_void,
+    ) -> bool;
+
+    pub type CreateDebugUtilsMessenger = unsafe extern "system" fn(
+        Instance,
+        *const DebugUtilsMessengerCreateInfo,
+        *const c_void,
+        *mut DebugUtilsMessenger,
+    ) -> Result;
+
+    pub unsafe extern "system" fn debug_utils_messenger_callback(
+        message_severity: c_uint,
+        message_type: c_uint,
+        callback_data: *const DebugUtilsMessengerCallbackData,
+        user_data: *const c_void,
+    ) -> bool {
+        let callback_data = callback_data.as_ref().unwrap();
+
+        let f = mem::transmute::<_, super::DebugUtilsMessengerCallback>(user_data);
+
+        let message = CStr::from_ptr(callback_data.message)
+            .to_string_lossy()
+            .into_owned();
+
+        let exposed_callback_data = super::DebugUtilsMessengerCallbackData {
+            message_severity,
+            message_type,
+            message: &message,
+        };
+
+        f(&exposed_callback_data)
+    }
 
     #[link(name = "vulkan")]
     #[allow(non_snake_case)]
@@ -74,6 +197,7 @@ mod ffi {
             allocator: *const c_void,
             instance: *mut Instance,
         ) -> Result;
+        pub fn vkGetInstanceProcAddr(instance: Instance, name: *const c_char) -> *const c_void;
     }
 }
 
@@ -81,9 +205,23 @@ pub const KHR_SURFACE: &str = "VK_KHR_surface";
 pub const KHR_XLIB_SURFACE: &str = "VK_KHR_xlib_surface";
 
 pub const EXT_DEBUG_REPORT: &str = "VK_EXT_debug_report";
+pub const EXT_DEBUG_UTILS: &str = "VK_EXT_debug_utils";
 
 pub const LAYER_KHRONOS_VALIDATION: &str = "VK_LAYER_KHRONOS_validation";
 pub const LAYER_LUNARG_STANDARD_VALIDATION: &str = "VK_LAYER_LUNARG_standard_validation";
+
+pub const DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE: u32 = 0x00000001;
+pub const DEBUG_UTILS_MESSAGE_SEVERITY_INFO: u32 = 0x00000010;
+pub const DEBUG_UTILS_MESSAGE_SEVERITY_WARNING: u32 = 0x00000100;
+pub const DEBUG_UTILS_MESSAGE_SEVERITY_ERROR: u32 = 0x00001000;
+
+pub const DEBUG_UTILS_MESSAGE_TYPE_GENERAL: u32 = 0x00000001;
+pub const DEBUG_UTILS_MESSAGE_TYPE_VALIDATION: u32 = 0x00000002;
+pub const DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE: u32 = 0x00000004;
+
+pub type Instance = ffi::Instance;
+pub type DebugUtilsMessenger = ffi::DebugUtilsMessenger;
+pub type DebugUtilsMessengerCallback = fn(&DebugUtilsMessengerCallbackData) -> bool;
 
 #[derive(Clone, Copy, Debug)]
 pub enum Error {
@@ -125,8 +263,6 @@ impl From<Version> for u32 {
     }
 }
 
-pub type Instance = ffi::Instance;
-
 #[derive(Clone, Copy)]
 pub struct ApplicationInfo<'a> {
     pub application_name: &'a str,
@@ -141,6 +277,21 @@ pub struct InstanceCreateInfo<'a> {
     pub application_info: &'a ApplicationInfo<'a>,
     pub extensions: &'a [&'a str],
     pub layers: &'a [&'a str],
+    pub debug_utils: Option<DebugUtilsMessengerCreateInfo>,
+}
+
+#[derive(Clone, Copy)]
+pub struct DebugUtilsMessengerCreateInfo {
+    pub message_severity: u32,
+    pub message_type: u32,
+    pub user_callback: DebugUtilsMessengerCallback,
+}
+
+#[derive(Clone, Copy)]
+pub struct DebugUtilsMessengerCallbackData<'a> {
+    pub message_severity: u32,
+    pub message_type: u32,
+    pub message: &'a str,
 }
 
 pub fn create_instance(create_info: InstanceCreateInfo<'_>) -> Result<Instance, Error> {
@@ -187,9 +338,34 @@ pub fn create_instance(create_info: InstanceCreateInfo<'_>) -> Result<Instance, 
         .map(|string| string.as_ptr())
         .collect::<Vec<_>>();
 
+    let internal_debug_utils_messenger_create_info =
+        if let Some(create_info) = create_info.debug_utils {
+            let g = unsafe { mem::transmute(create_info.user_callback) };
+
+            let internal_create_info = ffi::DebugUtilsMessengerCreateInfo {
+                structure_type: ffi::StructureType::DebugUtilsMessengerCreateInfo,
+                p_next: ptr::null(),
+                flags: 0,
+                message_severity: create_info.message_severity as _,
+                message_type: create_info.message_type as _,
+                user_callback: ffi::debug_utils_messenger_callback,
+                user_data: g,
+            };
+
+            Some(internal_create_info)
+        } else {
+            None
+        };
+
+    let p_next = if let Some(create_info) = internal_debug_utils_messenger_create_info {
+        unsafe { mem::transmute::<_, _>(&create_info) }
+    } else {
+        ptr::null()
+    };
+
     let internal_create_info = ffi::InstanceCreateInfo {
         structure_type: ffi::StructureType::InstanceCreateInfo,
-        p_next: ptr::null(),
+        p_next,
         flags: 0,
         application_info: &internal_application_info,
         enabled_layer_count: create_info.layers.len() as _,
@@ -218,6 +394,53 @@ pub fn create_instance(create_info: InstanceCreateInfo<'_>) -> Result<Instance, 
         ffi::Result::LayerNotPresent => Err(Error::LayerNotPresent),
         ffi::Result::ExtensionNotPresent => Err(Error::ExtensionNotPresent),
         ffi::Result::IncompatibleDriver => Err(Error::IncompatibleDriver),
+        _ => panic!("unexpected result"),
+    }
+}
+
+//TODO implement allocator
+pub fn create_debug_utils_messenger(
+    instance: Instance,
+    create_info: DebugUtilsMessengerCreateInfo,
+) -> Result<DebugUtilsMessenger, Error> {
+    let f_name = CStr::from_bytes_with_nul(b"vkCreateDebugUtilsMessengerEXT\0").unwrap();
+
+    let f = unsafe { ffi::vkGetInstanceProcAddr(instance, f_name.as_ptr()) };
+
+    if f == ptr::null() {
+        return Err(Error::ExtensionNotPresent);
+    }
+
+    let f = unsafe { mem::transmute::<_, ffi::CreateDebugUtilsMessenger>(f) };
+
+    let g = unsafe { mem::transmute(create_info.user_callback) };
+
+    let internal_create_info = ffi::DebugUtilsMessengerCreateInfo {
+        structure_type: ffi::StructureType::DebugUtilsMessengerCreateInfo,
+        p_next: ptr::null(),
+        flags: 0,
+        message_severity: create_info.message_severity as _,
+        message_type: create_info.message_type as _,
+        user_callback: ffi::debug_utils_messenger_callback,
+        user_data: g,
+    };
+
+    let mut internal_debug_utils_messenger = MaybeUninit::<ffi::DebugUtilsMessenger>::uninit();
+
+    let result = unsafe {
+        f(
+            instance,
+            &internal_create_info,
+            ptr::null(),
+            internal_debug_utils_messenger.as_mut_ptr(),
+        )
+    };
+
+    let internal_debug_utils_messenger = unsafe { internal_debug_utils_messenger.assume_init() };
+
+    match result {
+        ffi::Result::Success => Ok(internal_debug_utils_messenger),
+        ffi::Result::OutOfHostMemory => Err(Error::OutOfHostMemory),
         _ => panic!("unexpected result"),
     }
 }
